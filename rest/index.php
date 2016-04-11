f4987b02aa8758b25dc500d977779bdae8440a55
Richard Gunning <rjgunning@gmail.com>
2016-04-11 03:06:24 +0100

    REST complete chore(protractor): update to use protractor v2.1.0

--- rest/index.php
+++ rest/index.php
@@ -10,10 +10,10 @@ require 'vendor/autoload.php';
 require 'puntsController.php';
 
 spl_autoload_register(); // don't load our classes unless we use them
-var_dump(get_declared_classes());
+
 $mode = 'production'; // 'debug' or 'production'
 $server = new \Jacwright\RestServer\RestServer($mode);
-// $server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
+$server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
 
 $server->addClass('puntsController');
 //$server->addClass('ProductsController', '/products'); // adds this as a base to all the URLs in this class
