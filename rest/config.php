905120d178dedd8027fb8ab23172aefd16c7eb78
Richard Gunning <rjgunning@gmail.com>
2016-04-11 03:06:24 +0100

    REST complete chore(protractor): update to use protractor v2.1.0

---
+++ rest/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rjgun
+ * Date: 10/04/2016
+ * Time: 23:33
+ */
+// Define configuration
+define("DB_HOST", "localhost");
+define("DB_USER", "clarepunts");
+define("DB_PASS", "diphmif7");
+define("DB_NAME", "clarepunts");
+define("PREFIXNAME", "");
