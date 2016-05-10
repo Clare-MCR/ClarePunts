356685643055ff12597a9086ab92f5a5c304d5d1
RGunning <rjgunning@gmail.com>
2016-05-10 22:24:48 +0100

    update

--- src/client/app/core/ngConstants.js
+++ src/client/app/core/ngConstants.js
@@ -1,10 +1,10 @@
 /* jshint ignore:start */
-(function () {
-  "use strict";
+(function(){
+"use strict";
 
-  angular.module("app.core")
+angular.module("app.core")
 
-    .constant("version", "0.2.1")
-  ;
+.constant("version", "1.0.0")
+;
 })();
 /* jshint ignore:end */
