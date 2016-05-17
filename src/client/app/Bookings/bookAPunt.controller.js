a5e6be2e4ebd0dba53d82e96d8e4e36a1e95ef7d
Richard Gunning <rjgunning@gmail.com>
2016-05-17 20:33:27 +0100

    allow Admins to book for others

--- src/client/app/Bookings/bookAPunt.controller.js
+++ src/client/app/Bookings/bookAPunt.controller.js
@@ -274,13 +274,21 @@
     }
 
     function userBookingOnDay(crsid) {
-      return vm.bookings.filter(function (booking) {
-          var today0 = new Date(vm.dt);
-          var today24 = new Date(vm.dt);
-          today0.setUTCHours(0, 0, 0, 0);
-          today24.setUTCHours(24, 0, 0, 0);
-          return new Date(booking.timeTo) >= today0 && new Date(booking.timeFrom) < today24 && booking.booker === crsid;
-        }).length !== 0;
+      var bookings = vm.bookings.filter(function (booking) {
+        var today0 = new Date(vm.dt);
+        var today24 = new Date(vm.dt);
+        today0.setUTCHours(0, 0, 0, 0);
+        today24.setUTCHours(24, 0, 0, 0);
+        return new Date(booking.timeTo) >= today0 && new Date(booking.timeFrom) < today24 && booking.booker === crsid;
+      });
+      if (bookings.length !== 0) {
+        vm.canBook = false;
+        vm.bookingErrorMessage = 'Users are restricted to 1 booking per day!';
+      } else {
+        vm.canBook = true;
+        vm.bookingErrorMessage = '';
+      }
+      return vm.canBook;
     }
 
     function conflictBookings() {
@@ -350,9 +358,7 @@
         return false;
       }
       // check for bookings on day
-      if (userBookingOnDay(vm.form.booker)) {
-        vm.bookingErrorMessage = 'Users are restricted to 1 booking per day!';
-        vm.canBook = false;
+      if (!userBookingOnDay(vm.form.booker)) {
         return false;
       }
       // Check if the user has any upcoming bookings
@@ -387,10 +393,7 @@
     function changeInDate() {
       vm.form.timeFrom = new Date(vm.form.timeFrom)
         .setUTCFullYear(vm.dt.getUTCFullYear(), vm.dt.getUTCMonth(), vm.dt.getUTCDate());
-      if (userBookingOnDay(vm.form.booker)) {
-        vm.bookingErrorMessage = 'Users are restricted to 1 booking per day!';
-        vm.canBook = false;
-      }
+      userBookingOnDay(vm.form.booker);
     }
 
     function resetForm() {
